4261c99a89c3373af0242b24bb9b3b490c3523ce
Endjunjor <endjunjor@gmail.com>
2020-08-12 23:36:12 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,25 @@
 import discord
-import os
+from discord.ext import commands
+import asyncio
+import random
 
-client = discord.Client()
+bot = commands.Bot(command_prefix="#")
 
-@client.event
+@bot.event
 async def on_ready():
-	print("Bot is ready")
+	print("Online")
 	
-client.run(os.environ['Token'])
+	try:
+		home = bot.get_guild(658117203866025995)
+		role= home.get_role(743201327055634434)
+		while True:
+			color = [0xff0000, 0x000bff, 0x28ff00, 0xedff00, 0xff0084, 0xff7f00]
+			r = random.choice(color)
+			colors = discord.Colors(r)
+			await role.edit(colour=colors)
+			await asyncio.sleep(20)
+	except Exeptions as error:
+		print(error)
+
+
+bot.run(Token)
